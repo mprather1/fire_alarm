43bce681a35455326f2872e564f68e8ad473a297
Michael Prather <mprather1@gmail.com>
2016-05-12 19:19:07 -0500

    added authentication

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+from functools import wraps
+
 app = Flask(__name__)
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 GPIO.setmode(GPIO.BCM)
 
@@ -14,6 +34,7 @@ for pin in pins:
   GPIO.output(pin, GPIO.LOW)
 
 @app.route("/")
+@requires_auth
 def main():
   for pin in pins:
     pins[pin]['state'] = GPIO.input(pin)
@@ -24,6 +45,7 @@ def main():
   return render_template('index.html', **templateData)
 
 @app.route("/<changePin>/<action>")
+@requires_auth
 def action(changePin, action):
   changePin = int(changePin)
   deviceName = pins[changePin]['name']
